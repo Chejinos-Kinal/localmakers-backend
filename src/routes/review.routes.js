d4aa442fa832a9ab0e79da4320e8f6770bfd8e60
jperez-2022076 <jperez-2022076@kinal.edu.gt>
2024-07-18 23:19:19 -0600

    Review admin

--- src/routes/review.routes.js
+++ src/routes/review.routes.js
@@ -3,6 +3,7 @@
 import { Router } from 'express';
 import {
   getReview,
+  getReviewAdmin,
   getReviewProfesionl,
   newReview,
 } from '../controllers/review.controller.js';
@@ -12,6 +13,7 @@ const api = Router();
 
 api.post('/new', [validateJwt], newReview);
 api.get('/get/:userProfessional', [validateJwt], getReview);
+api.get('/getAdminReview/:emailProfesion', [validateJwt], getReviewAdmin);
 api.get('/getProfesional', [validateJwt], getReviewProfesionl);
 
 export default api;
